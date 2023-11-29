6d8ee9f7286897f3e0ba5f3a96c6acc59b81b9ef
JoshT1984 <warlord84502@yahoo.com>
2023-11-29 12:22:53 -0600

    Photos added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-// fetch('https://fakestoreapi.com/products')
-//             .then(res=>res.json())
-//             .then(json=>console.log(json));
+// $.get("https://fakestoreapi.com/products", (data) => {
+//   console.log(data);
+// });
+
+let $test = $(".test");
+
+$test.on("click", function () {
+  console.log("IT WORKS");
+});
+
+
+$(document).ready(function () {
+ 
+    // Adding click event on id div-1
+    // if it clicked then anonymous
+    // function will be called
+    $('#div-1').click(function () {
+
+        // Load the external html
+        // here this refers to 
+        // current selector
+        $(this).load('div-1.html');
+    });
