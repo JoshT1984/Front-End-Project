210b171456c7cfb87809932d2092c4abdc6351d4
JoshT1984 <warlord84502@yahoo.com>
2023-11-29 15:14:04 -0600

    rotating click icon

--- js/index.js
+++ js/index.js
@@ -2,22 +2,45 @@
 //   console.log(data);
 // });
 
-let $test = $(".test");
+// $test.on("click", function () {
+//   console.log("IT WORKS");
+// });
+// animation();
+render();
 
-$test.on("click", function () {
-  console.log("IT WORKS");
-});
+function render() {
+  // changeDocs();
+}
 
+// function changeDocs() {
+//   $(document).ready(function () {
+//     let $test = $(".main-image");
+//     // Adding click event on id div-1
+//     // if it clicked then anonymous
+//     // function will be called
+//     $test.on("click", function () {
+//       // Load the external html
+//       // here this refers to
+//       // current selector
+//       $(this).load("test.html");
+//     });
+//   });
+// }
 
-$(document).ready(function () {
- 
-    // Adding click event on id div-1
-    // if it clicked then anonymous
-    // function will be called
-    $('#div-1').click(function () {
+var rotateLine = 0;
 
-        // Load the external html
-        // here this refers to 
-        // current selector
-        $(this).load('div-1.html');
-    });
+  let $board = $(".skate-icon").click(function () {
+    rotateLine += 45;
+    $board.css({ transform: "rotate(" + rotateLine + "deg" });
+  });
+
+// function animation() {
+//
+//   const frameHeight = 102;
+//   const frames = 15;
+//   let frame = 0;
+//   setInterval(function () {
+//     const frameOffset = (++frame % frames) * -frameHeight;
+//     $div.css("background-position", "0px " + frameOffset + "px");
+//   }, 100);
+// }
